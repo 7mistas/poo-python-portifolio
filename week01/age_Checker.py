525138c8788f8ea10dd940ac7a9a9fba2eb49503
Mista <levy.7mx@gmail.com>
2025-10-17 14:55:10 -0300

    Incluse try block

--- week01/age_Checker.py
+++ week01/age_Checker.py
@@ -4,12 +4,17 @@ def pode_dirigir(idade):
     # Idade mínima para dirigir.
     IDADE_MINIMA = 18
 
+    # Retorno do valor em 
+    return idade >= IDADE_MINIMA
+
+#  Tentativa da entrada da idade e tratamento de erro.
+try:
+    # Entrada da idade por input no CLI.
+    sua_idade = int(input("Digite sua idade: "))
     # Compara o valor de idade com a meta mínima.
-    if idade >= IDADE_MINIMA:
-        print("Voce pode dirigir")
+    if pode_dirigir(sua_idade):
+        print("Voce pode dirigir!")
     else:
         print("Você não pode dirigir! ")
-# Entrada da idade do usuário.
-sua_idade = int(input("Digite sua idade: "))
-# Executa a função.
-pode_dirigir(sua_idade)
+except ValueError:
+    print("Erro: Somente numeros são válidos!")
