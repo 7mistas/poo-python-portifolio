8c4d546912026be4fd69a7c7ea40d5d238864e8d
Mista <levy.7mx@gmail.com>
2025-11-06 14:04:48 -0300

    add auth.py

---
+++ week06/auth.py
@@ -0,0 +1,39 @@
+from db_auth import Database_Auth
+from typing import Optional
+
+class Autenticacao:
+    def __init__(self):
+        self.db_auth = Database_Auth()
+        self.usuario_logado = Optional[int] = None
+        self.id_logado = Optional[int] = None
+
+    def registrar(self, usuario: str, senha: str, email: "") -> Tuple[bool, str]:
+        return self.db_auth.registrar(usuario, senha, email)
+
+    def login(self, usuario: str, senha: str) -> Tuple[bool, str]:
+        autenticado, user_id = self.db.autenticar_usuario(usuario, senha)
+
+        if autenticado:
+            self.id_logado = user_id
+            self.usuario_logado = usuario
+            return True, f"Bem vindo {usuario}! ID: {user_id}"
+
+        else:
+            return False, f"Usuário ou senha incorretos!"
+
+    def logout(self):
+        if self.usuario_logado:
+            print(f"Usuario {self.usuario_logado} desconectado!")
+
+        self.id_logado = None
+        self.usuario_logado = None
+
+    def esta_logado(self) -> bool:
+        return self.usuario_logado is not None
+
+    def exibir_info_usuario(self) -> Optional[dict]:
+        if self.id_logado:
+            return self.db.obter_info(self.id_logado)
+        return None
+
+
